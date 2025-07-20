59066d0a052536abb6943bb1a35f408e16973e38
Danilo Guerra <120229652+Danflea@users.noreply.github.com>
2025-07-20 00:09:06 -0500

    Add files via upload

---
+++ fotos_insert.sql
@@ -0,0 +1,17 @@
+CREATE TEMPORARY TABLE alumnos_temp (
+    codigo VARCHAR(20),
+    foto_url TEXT
+);
+
+LOAD DATA INFILE 'imagen_nombre.csv'
+INTO TABLE alumnos_temp
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(codigo, foto_url);
+
+
+UPDATE alumnos a
+JOIN alumnos_temp t ON a.codigo = t.codigo
+SET a.foto_url = t.foto_url;
